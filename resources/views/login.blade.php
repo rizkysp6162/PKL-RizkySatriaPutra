230f7ae3e4480e6a206ff8ad16000703efad46c5
rizkysp6162 <rizkysp6162@gmail.com>
2024-06-07 19:00:06 +0700

    0.1 login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -8,15 +8,13 @@
     @vite('resources/css/app.css')
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
 </head>
-<body class="h-full">
-    <div class="flex min-h-full flex-row justify-center bg-local "  style="background-image:url(https://visualmodo.com/wp-content/uploads/2022/08/5-Best-Gaming-Website-Templates-Design-Made-Easy.png)">
-        <div class="">
-            <img class="" src="" alt="Your Company">
-        </div>
+<body class="h-full bg-purple-700	">
+    <div class="flex min-h-full flex-row justify-center ">
+        <img class="" src="https://visualmodo.com/wp-content/uploads/2022/08/5-Best-Gaming-Website-Templates-Design-Made-Easy.png" alt="Your Company">
         <!-- Login Kanan-->
-        <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+        <div class="bg-white flex min-h-full flex-col justify-center p-20 xl:px-24">
             <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-              <img class="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company">
+              <img class="mx-auto h-10 w-auto" src="https://moogie.id/assets/images/1680512565_28bd505aa10e4d552ac2.png" alt="Your Company">
               <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in to your account</h2>
             </div>
         
@@ -32,9 +30,6 @@
                 <div>
                   <div class="flex items-center justify-between">
                     <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
-                    <div class="text-sm">
-                      <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
-                    </div>
                   </div>
                   <div class="mt-2">
                     <input id="password" name="password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
@@ -42,13 +37,13 @@
                 </div>
         
                 <div>
-                  <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
+                  <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Log in</button>
                 </div>
               </form>
         
               <p class="mt-10 text-center text-sm text-gray-500">
-                Not a member?
-                <a href="#" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Start a 14 day free trial</a>
+                Don't have an account?
+                <a href="/register" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Register</a>
               </p>
             </div>
           </div>

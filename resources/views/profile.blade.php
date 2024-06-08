4e6f4142597eff7d4048bc031e734b3355805d72
rizkysp6162 <rizkysp6162@gmail.com>
2024-06-08 17:35:14 +0700

    Update profile.blade.php

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -8,7 +8,7 @@
     @vite('resources/css/app.css')
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
 </head>
-<body class="bg-zinc-100	">
+<body class="	">
     <nav class="bg-gray-800 " x-data="{ isOpen: false }">
         
     </div>
